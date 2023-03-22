a10c8d5c9ef280dc5d5ae115676e71d5e84b7237
Lauro Mello <71044420+mellolauro@users.noreply.github.com>
2023-03-21 22:37:26 -0300

    Update db_dados_email.sql

--- db_dados_email.sql
+++ db_dados_email.sql
@@ -1,13 +1,17 @@
 create schema msg_email;
 
-CREATE TABLE `msg_email`.`arquivo_email` (
-  `nome_arquivo` VARCHAR(255) NOT NULL,
-  `remetente` VARCHAR(255) NOT NULL,
-  `destinatario` VARCHAR(255) NOT NULL,
-  `dt_hora` VARCHAR(255) NOT NULL,
-  `conteudo` TEXT NOT NULL,
-  PRIMARY KEY (`nome_arquivo`));
+CREATE DATABASE msg_email;
+
+use msg_email;
+
+CREATE TABLE msg_email.arquivo_email (
+  nome_arquivo VARCHAR(255) NOT NULL,
+  remetente VARCHAR(255) NOT NULL,
+  destinatario VARCHAR(255) NOT NULL,
+  dt_hora VARCHAR(255) NOT NULL,
+  conteudo TEXT NOT NULL,
+  PRIMARY KEY (nome_arquivo));
   
-  CREATE TABLE `msg_email`.`palavra_chave` (
-  `chave` VARCHAR(255) NOT NULL,
-  `tipo` VARCHAR(255) NOT NULL);
+  CREATE TABLE msg_email.palavra_chave (
+  chave VARCHAR(255) NOT NULL,
+  tipo VARCHAR(255) NOT NULL);
